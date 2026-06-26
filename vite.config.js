a316fe64035e627bca70eaff839d664d6a779200
snail <2352726120@qq.com>
2026-06-26 09:17:00 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/snail-resume/',
+  base: '/snail-resume/', // 仓库名称
   build: {
     outDir: 'docs'
   }
